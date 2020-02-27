02465330e7887c8e3a688d6a93f9c677afd2c998
Pedro <pedrohba18@gmail.com>
2020-02-27 13:49:01 -0300

    loading indicator quando carrega stars

--- src/pages/User/index.js
+++ src/pages/User/index.js
@@ -2,9 +2,22 @@
 /* eslint-disable react/state-in-constructor */
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
+import {ActivityIndicator} from 'react-native';
 
 import api from '../../services/api';
-import {Container, Header, Avatar, Name, Bio} from './styles';
+import {
+    Container,
+    Header,
+    Avatar,
+    Name,
+    Bio,
+    Stars,
+    Starred,
+    OwnerAvatar,
+    Info,
+    Title,
+    Author,
+} from './styles';
 
 export default class User extends Component {
     static routeParams = ({route}) => ({
@@ -20,19 +33,21 @@ export default class User extends Component {
 
     state = {
         stars: [],
+        loading: false,
     };
 
     async componentDidMount() {
+        this.setState({loading: true});
         const {route} = this.props;
         const {user} = route.params;
         const response = await api.get(`/users/${user.login}/starred`);
-        this.setState({stars: response.data});
+        this.setState({stars: response.data, loading: false});
     }
 
     render() {
         const {route} = this.props;
         const {user} = route.params;
-        const {stars} = this.state;
+        const {stars, loading} = this.state;
 
         return (
             <Container>
@@ -41,6 +56,26 @@ export default class User extends Component {
                     <Name>{user.name}</Name>
                     <Bio>{user.bio}</Bio>
                 </Header>
+
+                {loading ? (
+                    <ActivityIndicator color="#fff" size={100} />
+                ) : (
+                    <Stars
+                        data={stars}
+                        keyExtractor={star => String(star.id)}
+                        renderItem={({item}) => (
+                            <Starred>
+                                <OwnerAvatar
+                                    source={{uri: item.owner.avatar_url}}
+                                />
+                                <Info>
+                                    <Title>{item.name}</Title>
+                                    <Author>{item.owner.login}</Author>
+                                </Info>
+                            </Starred>
+                        )}
+                    />
+                )}
             </Container>
         );
     }
