c930365a8cf56ece76efdda349e68e9b63c8bd5c
Pedro <pedrohba18@gmail.com>
2020-02-27 14:59:17 -0300

    load de mais páginas no scroll down

--- src/pages/User/index.js
+++ src/pages/User/index.js
@@ -3,6 +3,8 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import {ActivityIndicator} from 'react-native';
+import {WebView} from 'react-native-webview';
+
 
 import api from '../../services/api';
 import {
@@ -34,6 +36,8 @@ export default class User extends Component {
     state = {
         stars: [],
         loading: false,
+        page: 1,
+        refreshing: false,
     };
 
     async componentDidMount() {
@@ -44,10 +48,32 @@ export default class User extends Component {
         this.setState({stars: response.data, loading: false});
     }
 
+    refreshList = () => {
+        this.setState({refreshing: true});
+    };
+
+    async loadMore() {
+        this.setState({refreshing: true});
+        const {stars, page} = this.state;
+        const {route} = this.props;
+        const {user} = route.params;
+
+        const response = await api.get(`/users/${user.login}/starred`, {
+            params: {page: page + 1},
+        });
+
+        this.setState({
+            stars: page >= 2 ? [...stars, ...response.data] : response.data,
+            page: page + 1,
+            loading: false,
+            refreshing: false,
+        });
+    }
+
     render() {
         const {route} = this.props;
         const {user} = route.params;
-        const {stars, loading} = this.state;
+        const {stars, loading, refreshing} = this.state;
 
         return (
             <Container>
@@ -61,18 +87,26 @@ export default class User extends Component {
                     <ActivityIndicator color="#fff" size={100} />
                 ) : (
                     <Stars
+                        onEndReachedThreshold={0.2} // Carrega mais itens quando chegar em 20% do fim
+                        onEndReached={() => this.loadMore()} // Função que carrega mais itens
+                        onRefresh={this.refreshList}
+                        refreshing={refreshing}
                         data={stars}
                         keyExtractor={star => String(star.id)}
                         renderItem={({item}) => (
-                            <Starred>
-                                <OwnerAvatar
-                                    source={{uri: item.owner.avatar_url}}
-                                />
-                                <Info>
-                                    <Title>{item.name}</Title>
-                                    <Author>{item.owner.login}</Author>
-                                </Info>
-                            </Starred>
+                            <>
+                                <Starred>
+                                    <OwnerAvatar
+                                        source={{
+                                            uri: item.owner.avatar_url,
+                                        }}
+                                    />
+                                    <Info>
+                                        <Title>{item.name}</Title>
+                                        <Author>{item.owner.login}</Author>
+                                    </Info>
+                                </Starred>
+                            </>
                         )}
                     />
                 )}
